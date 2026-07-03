babedc5430c0b6b260f95e1a85d854224e5e3df0
Kalicharan <tamaralakalicharan3@gmail.com>
2026-07-03 16:33:36 +0530

    Initial commit

---
+++ EmployeePayroll.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class EmployeePayroll {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Employee Name: ");
+        String empName = scanner.nextLine();
+
+        System.out.print("Enter Employee ID: ");
+        String empId = scanner.nextLine();
+
+        System.out.print("Enter Basic Salary: ");
+        double basicSalary = scanner.nextDouble();
+        double hra = basicSalary * 0.40; // 40% HRA
+        double da = basicSalary * 0.15;  // 15% DA
+        double pf = basicSalary * 0.12;  // 12% PF deduction
+
+        double grossSalary = basicSalary + hra + da - pf;
+
+    
+        System.out.println("\n=============================================");
+        System.out.println("              EMPLOYEE PAYSLIP               ");
+        System.out.println("=============================================");
+        System.out.printf("%-20s : %s\n", "Employee ID", empId);
+        System.out.printf("%-20s : %s\n", "Employee Name", empName);
+        System.out.println("---------------------------------------------");
+        System.out.printf("%-20s : %.2f\n", "Basic Salary", basicSalary);
+        System.out.printf("%-20s : + %.2f\n", "HRA (40%)", hra);
+        System.out.printf("%-20s : + %.2f\n", "DA (15%)", da);
+        System.out.printf("%-20s : - %.2f\n", "PF (12%)", pf);
+        System.out.println("---------------------------------------------");
+        System.out.printf("%-20s : %.2f\n", "GROSS SALARY", grossSalary);
+        System.out.println("=============================================");
+
+        scanner.close();
+    }
+}
---
+++ README.md.txt
@@ -0,0 +1,16 @@
+# Employee Payroll System
+
+## Description
+A Java console application that calculates employee salary.
+
+## Features
+- Employee Name
+- Employee ID
+- Basic Salary
+- HRA Calculation
+- DA Calculation
+- PF Calculation
+- Gross Salary
+
+## Language
+Java
